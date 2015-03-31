ef44b294ba9e24ebb83c31eb97d42d2f0bb5e218
Mursil Sayed <mursilsayed@gmail.com>
2015-03-31 17:31:39 +0500

    Property Initialization done via Application context

--- src/org/mursil/spring/practice/Triangle.java
+++ src/org/mursil/spring/practice/Triangle.java
@@ -2,8 +2,18 @@ package org.mursil.spring.practice;
 
 public class Triangle {
 
+	private String type;
+	
 	public void draw(){
 		
-		System.out.println("Triangle Drawn");
+		System.out.println(this.getType()+ " Triangle Drawn");
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 }
