fb6b4a38b3963384a7c0b02fc3ae55ade13d0e88
Mursil Sayed <mursilsayed@gmail.com>
2015-03-31 18:36:02 +0500

    Adding Constructor Injection with overloaded constructor

--- src/org/mursil/spring/practice/Triangle.java
+++ src/org/mursil/spring/practice/Triangle.java
@@ -3,17 +3,30 @@ package org.mursil.spring.practice;
 public class Triangle {
 
 	private String type;
+	private int height;
+	public int getHeight() {
+		return height;
+	}
+	public Triangle(String type){
+		
+		this.type = type;
+	}
+    public Triangle(String type, int height){
+		
+		this.type = type;
+		this.height= height;
+	}
 	
 	public void draw(){
 		
-		System.out.println(this.getType()+ " Triangle Drawn");
+		System.out.println(this.getType()+ " Triangle Drawn with height "+this.getHeight());
 	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
+//	public void setType(String type) {
+//		this.type = type;
+//	}
 }
