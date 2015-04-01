099ca949091977265ed0cf0a29b0609b614d7055
Mursil Sayed <mursilsayed@gmail.com>
2015-04-01 18:33:13 +0500

    Added few more JSR-250 Annotation usage to the codebase. but these annotations are not working out of the box.

--- src/org/mursil/spring/practice/Circle.java
+++ src/org/mursil/spring/practice/Circle.java
@@ -1,5 +1,9 @@
 package org.mursil.spring.practice;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -13,6 +17,7 @@ public class Circle implements Shape {
 
 	@Autowired
 	@Qualifier("circleRelated")
+	//@Resource(name="centre")
 	public void setCentre(Point centre) {
 		this.centre = centre;
 	}
@@ -22,5 +27,19 @@ public class Circle implements Shape {
 		System.out.println(centre);
 		
 	}
+	
+	@PostConstruct
+	public void onDestroy()  {
+		System.out.println("Circle: Custom onDestroy");
+		
+	}
+	
+	@PreDestroy
+	public void OnInitCircle() 
+	{
+		System.out.println("Circle: Custom OnInit");
+	}
+	
+	
 
 }
