87240e107541197dbd8420f68ca5e398ba33c905
Mursil Sayed <mursilsayed@gmail.com>
2015-04-14 12:08:43 +0500

    Adding Support for Building war(along with jar) file so that not only the project can run with Embedded tomcat container but can also run inside another Java Web Profile Container

--- SpringBootSample/src/main/java/org/mursil/spring/SpringBootApp.java
+++ SpringBootSample/src/main/java/org/mursil/spring/SpringBootApp.java
@@ -2,11 +2,18 @@ package org.mursil.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan()
 @EnableAutoConfiguration
-public class SpringBootApp {
+public class SpringBootApp  extends SpringBootServletInitializer  {
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SpringBootApp.class);
+	    }
 
 	public static void main(String[] args) {
 		System.out.print("Hello World");
