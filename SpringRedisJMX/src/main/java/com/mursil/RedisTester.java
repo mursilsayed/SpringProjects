fdc7e5cdf77ea003762108a0534eafa4d59b3ceb
Mursil Sayed <ahmad.mursil@hbl.com>
2016-06-25 21:39:38 +0500

    used CacheEvit Method to Clear the Cache

--- SpringRedisJMX/src/main/java/com/mursil/RedisTester.java
+++ SpringRedisJMX/src/main/java/com/mursil/RedisTester.java
@@ -1,6 +1,7 @@
 package com.mursil;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jmx.export.annotation.ManagedOperation;
@@ -30,11 +31,20 @@ public class RedisTester {
 	
 	
 	@ManagedOperation
-	@Cacheable("mursil2")
-	public String getComplexValue(){
+	@Cacheable(cacheNames="mursil2",key="#value")
+	public String getComplexValue(int value){
 		
 		System.out.print("Complex Value Generated");
-		return 	"Complex Value";
+		return 	"Complex Value"+value;
+		
+	}
+	
+	@ManagedOperation
+	@CacheEvict(cacheNames="mursil2", allEntries=true)
+	public void evictCacheValues(){
+		
+		System.out.print("Evicting All Cache values");
+		//return 	"Complex Value"+value;
 		
 	}
 	
